86117bce56362b427125934d5d682cf16dcecf6d
ilay58 <ngp.ilay4gmail.com>
2023-08-27 08:37:49 +0300

    restore files

--- src/begin.js
+++ src/begin.js
@@ -2,29 +2,29 @@ import React, { useState, useEffect } from 'react';
 import './begin.css';
 import backgroundImage from './pic/what.png';
 import raccoon from './pic/raccoon.png';
+import ButtonComponent from './buttonBegin';
+
 
 function Begin() {
-    const [isVisible, setIsVisible] = useState(false);
+   // eslint-disable-next-line no-unused-vars
+const [isVisible, setIsVisible] = useState(false);
+
 
     useEffect(() => {
         setIsVisible(true);
     }, []);
 
     return (
+        <section>
         <div className="container">
             <img className="background-image" src={backgroundImage} alt="background" />
             <img className="overlay-image float" src={raccoon} alt="overlay" />
             <div className="top-left-images">
-                <h1 className="h1">REVENUE</h1>
-                <h1 className="hd1">RACCOON</h1>
+                <h1 className="h1 typing-text">REVENUE</h1>
+                <h1 className="hd1 typing-text">RACCOON</h1>
             </div>
-            <div className="button-container">
-                <div className="button">
-                    <div className="circle"></div>
-                    <div className="button-text">Begin</div>
-                </div>
-            </div>
-        </div>
+       <ButtonComponent/>
+        </div></section>
     );
 }
 
