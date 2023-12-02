5e03bc519892ce8ebda35a49c554915b345e8039
Yoel weisberg <yoekweisberg5@gmail.com>
2023-12-02 20:01:36 +0200

    fixed two erors

--- RevenueRacoonApp/babel.config.js
+++ RevenueRacoonApp/babel.config.js
@@ -1,24 +1,6 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/App.js', // Adjust the entry point based on your project structure
-  output: {
-    path: path.resolve(__dirname, 'dist'), // Adjust the output directory as needed
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
