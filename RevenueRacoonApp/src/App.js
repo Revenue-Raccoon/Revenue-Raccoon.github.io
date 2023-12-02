8c202e3890bf808ef3fd0add3cd076557f588699
Yoel weisberg <yoekweisberg5@gmail.com>
2023-12-02 20:12:48 +0200

    fixed problem

--- RevenueRacoonApp/src/App.js
+++ RevenueRacoonApp/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';  
 import { StyleSheet, View } from 'react-native';
 
-import Routes from '../functions/Routes.jsx';
+import Routes from '../src/functions/Routes';
 
 export default function App() {
 
