1bbee48d247d3fe3c4cc0efc2afc8f273fb9f34d
Yoel weisberg <yoekweisberg5@gmail.com>
2023-12-05 16:30:39 +0200

    working

--- RevenueRacoonApp/babel.config.js
+++ RevenueRacoonApp/babel.config.js
@@ -1,12 +1,1 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo', '@babel/preset-env', '@babel/preset-react'],
-    plugins: [
-      ["@babel/plugin-transform-private-methods", { "loose": true }],
-      ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
-      ["@babel/plugin-transform-class-properties", { "loose": true }]
-      // ... other plugins from .babelrc
-    ]
-  };
-};
+module.exports = {     presets: ['module:metro-react-native-babel-preset'],     plugins: [         [             'module:react-native-dotenv',             {                 moduleName: '@env',                 path: '.env',                 blacklist: null,                 whitelist: null,                 safe: false,                 allowUndefined: true,             },         ],     ], };
