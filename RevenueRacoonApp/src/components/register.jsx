3c00e6fa3ff2d9c38f636217eefc94571b5fa122
Yoel_Weisberg5 <thek57525@gmail.com>
2023-12-13 12:39:43 +0200

    fixed merge

--- RevenueRacoonApp/src/components/register.jsx
+++ RevenueRacoonApp/src/components/register.jsx
@@ -1,22 +1,34 @@
-import React, { useState }, { useState } from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, TextInput, Image } from 'react-native'; // Removed CheckBox from here
 import CheckBox from 'expo-checkbox';
 import styles from '../styles/registerStyle';
-import {LinearGradient} from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+// ... other necessary imports ..
 const Register = () => {
+  const [isSelected, setSelection] = useState(false);
   return (
     <View style={styles.container}>
-      <LinearGradient
-        colors={['rgba(143, 0, 255, 0.80)', 'rgba(0, 0, 0, 0)']}
+       <LinearGradient
+        colors={['rgba(143, 0, 255, 0.80)', 'rgba(0, 0, 0, 0)']} // הגדרת הצבעים ל-gradient
         style={styles.topGradient}
         start={{x: 0, y: 0}}
         end={{x: 0, y: 1}}
       >
         {/* יצירת גרדיאנט באמצעות LinearGradient */}
       </LinearGradient>
-      <View style={[styles.bottomGradient, { transform: [{ rotate: '179.91deg' }] }]}>
-        {/* יצירת גרדיאנט באמצעות שכבות של View */}
-      </View>
+      <LinearGradient
+      colors={[ 'rgba(143, 0, 255, 0.80)', 'rgba(0, 0, 0, 0)']}
+      style={styles.bottomGradient}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+    />
+    <TouchableOpacity  style={styles.button1}>
+      <Image
+        source={{ uri: 'https://i.ibb.co/MMxMb4b/image.png' }} // Replace with your image URL if needed
+        style={styles.image}
+      />
+    </TouchableOpacity>
       <Text style={styles.welcomeText}>
         Welcome to {'\n'}
         Revenue Raccoon! {'\n'}
@@ -24,46 +36,25 @@ const Register = () => {
       </Text>
 
       <View style={[styles.inputContainer, { top: 208.86 }]}>
-        <TextInput
-          placeholder="Username"
-          style={styles.input}
-          value={username}
-          onChangeText={(text) => setUsername(text)}
-        />
+        <TextInput placeholder="Username" style={styles.input} />
       </View>
       <View style={[styles.inputContainer, { top: 272.77 }]}>
-        <TextInput
-          placeholder="Email"
-          style={styles.input}
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
+        <TextInput placeholder="Email" style={styles.input} />
       </View>
       <View style={[styles.inputContainer, { top: 336.69 }]}>
-        <TextInput
-          placeholder="Password"
-          secureTextEntry={true}
-          style={styles.input}
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
+        <TextInput placeholder="Password" secureTextEntry={true} style={styles.input} />
       </View>
       <View style={[styles.inputContainer, { top: 400.61 }]}>
-        <TextInput
-          placeholder="Confirm Password"
-          secureTextEntry={true}
-          style={styles.input}
-          value={confirmPassword}
-          onChangeText={(text) => setConfirmPassword(text)}
-        />
+        <TextInput placeholder="Confirm Password" secureTextEntry={true} style={styles.input} />
       </View>
 
       <View style={styles.loginWithContainer}>
+        {/* Add icons or buttons for login with other services here */}
         <Text style={styles.loginWithText}>Or Login with</Text>
       </View>
       
 
-      <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
+      <TouchableOpacity style={styles.registerButton}>
         <Text style={styles.registerButtonText}>Agree and Register</Text>
       </TouchableOpacity>
 
